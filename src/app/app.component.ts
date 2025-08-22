b0fc0ad28c443d18187b39406af482096c152b5a
Erik Plachta <eplachta@gmail.com>
2025-08-22 18:58:53 -0400

    feat: add excel-aware nav and pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ExcelService } from './excel.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(public excel: ExcelService) {}
   title = 'excel-extension';
 }
